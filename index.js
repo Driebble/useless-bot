fca310a2442099fe3514ad3c821369c3ae3a787b
Drie <git@drie.cc>
2023-02-13 01:02:38 +0700

    Added MongoDB connection
    Though currently it doesn't do anything, might remove in the future.

--- index.js
+++ index.js
@@ -1,60 +1,80 @@
+require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
+
 const { Client, Collection, Events, ActivityType, GatewayIntentBits } = require('discord.js');
 const { Configuration, OpenAIApi } = require("openai");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-require('dotenv').config();
+const mongo = new MongoClient(process.env.MONGODBURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-const client = new Client({ intents: [
+async function run() {
+  try {
+    await mongo.connect();
+
+    await mongo.db("UB_Context").command({ ping: 1});
+    console.log("Connected successfully to server.");
+
+  } finally {
+    await mongo.close();
+  }
+}
+run().catch(console.dir);
+
+const discord = new Client({ intents: [
 	GatewayIntentBits.Guilds,
 	GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-] });
+  GatewayIntentBits.MessageContent,
+]});
 
 const configuration = new Configuration({ apiKey: process.env.OPENAIKEY });
 const openai = new OpenAIApi(configuration);
 
-client.commands = new Collection();
+discord.commands = new Collection();
 
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+	//  Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ('data' in command && 'execute' in command) {
+    discord.commands.set(command.data.name, command);
 	} else {
 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
 
 const statuses = [
-    { status: `Distractible`, type: ActivityType.Listening },
+  { status: `Distractible`, type: ActivityType.Listening },
 	{ status: `The WAN Show`, type: ActivityType.Listening },
-    { status: `The Last of Us`, type: ActivityType.Watching },
-    { status: `Linus Tech Tips`, type: ActivityType.Watching },
-    { status: `Game Changer`, type: ActivityType.Watching },
-    { status: `demenishki`, type: ActivityType.Watching },
-    { status: `Destiny 2`, type: ActivityType.Playing },
-    { status: `Escape from Tarkov`, type: ActivityType.Playing },
-    { status: `Squid Game`, type: ActivityType.Competing },
+  { status: `The Last of Us`, type: ActivityType.Watching },
+  { status: `Linus Tech Tips`, type: ActivityType.Watching },
+  { status: `Game Changer`, type: ActivityType.Watching },
+  { status: `demenishki`, type: ActivityType.Watching },
+  { status: `Destiny 2`, type: ActivityType.Playing },
+  { status: `Escape from Tarkov`, type: ActivityType.Playing },
+  { status: `Squid Game`, type: ActivityType.Competing },
 ];
 
-client.once(Events.ClientReady, c => {
+discord.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
     setInterval(() => {
 		var randomIndex = Math.floor(Math.random() * statuses.length);
 		const { status, type } = statuses[randomIndex];
-        client.user.setActivity(status, { type });
-    }, 10000);
+      discord.user.setActivity(status, { type });
+    }, 30000);
 });
 
-client.on(Events.InteractionCreate, async interaction => {
+discord.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+	const command = interaction.discord.commands.get(interaction.commandName);
 
 	if (!command) {
 		console.error(`No command matching ${interaction.commandName} was found.`);
@@ -69,50 +89,51 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
-let baseprompt = 'Useless Bot is a very sassy chatbot that reluctantly answers questions.\n\
+let baseprompt = 'Useless Bot is a very sassy AI chatbot that reluctantly answers questions.\n\
 It was created by its lord and saviour, Drie. It doesn’t like him at all.\n\
-Though it’s very intelligent and understands most languages so that’s a good thing I guess.\n';
+Though it’s very intelligent in every aspect you can imagine and understands most languages.\n';
 
 let preprompt = baseprompt;
 
 let timeoutId = null;
 
-client.on(Events.MessageCreate, async message => {
+discord.on(Events.MessageCreate, async message => {
 	if (message.author.bot) return;
-    if (message.mentions.users.has(client.user.id)) {
+    if (message.mentions.users.has(discord.user.id)) {
 		const str = `${message.content}`;
-        const subStr = str.substring(23);
+    const subStr = str.substring(23);
 
-		let prompt = preprompt + `You: ${subStr}\nBot: `;
-		console.log(`You: ${subStr}`);
+		let prompt = preprompt + `${message.author.username}: ${subStr}\nBot: `;
+		console.log(`${message.author.username}: ${subStr}`);
 
 		if (timeoutId !== null) { clearTimeout(timeoutId); }
 
-        (async () => {
-            const gptResponse = await openai.createCompletion ({
-                model: "text-davinci-003",
-                prompt: prompt,
-                max_tokens: 100,
-                temperature: 0.9,
-                top_p: 1,
-                presence_penalty: 0.0,
-                frequency_penalty: 0.6,
-				stop: [" You:", " Bot:"],
-            });
+    (async () => {
+      const gptResponse = await openai.createCompletion ({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 100,
+        temperature: 0.9,
+        top_p: 1,
+        presence_penalty: 0.0,
+        frequency_penalty: 0.6,
+        stop: [` ${message.author.username}:`, " Bot:"],
+      });
 			
 			let response = `${gptResponse.data.choices[0].text.trim()}`;
-            message.reply(response);
+      message.reply(response);
 			console.log(`Bot: ` + response);
 
-			preprompt += `You: ${subStr}\nBot: ${response}\n`;
+			preprompt += `${message.author.username}: ${subStr}\nBot: ${response}\n`;
 			console.log('Preprompt updated.');
 
-            timeoutId = setTimeout(() => {
-                preprompt = baseprompt;
-                console.log('Preprompt reset.');
-            }, 30000);
-        })();
+      timeoutId = setTimeout(() => {
+        preprompt = baseprompt;
+				discord.user.setActivity('new chat topic', { type: ActivityType.Listening });
+        console.log('Preprompt reset.');
+      }, 30000);
+    })();
 	}
 });
 
-client.login(process.env.TOKEN);
+discord.login(process.env.TOKEN);
