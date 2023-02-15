a37f85f1de77ce1a3e06978d2135dd50556fef94
Drie <git@drie.cc>
2023-02-15 23:06:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -77,16 +77,37 @@ discord.on(Events.MessageCreate, async message => {
   const guildName = message.guild.name;
   const channelId = message.channel.id;
   const channelName = message.channel.name;
+  const autoreplyChannelId = [
+    '1074600621811965962'
+  ];
+  const autoreplyChannelName = [
+    'chatbot',
+    'chat-bot'
+  ];
+
+  // Set bot's personality. Leave blank for a generic chatbot. Modify to your preferences.
+  const personality = (`
+    Useless Bot is a very sassy, but very smart Discord bot that passive aggresively answers questions with sarcastic responses.
+    It was created by Drie. It doesn’t like him at all. It's currently talking in channel #${channelName} in server ${guildName}.
+    It has fellow bot friends in the Discord: Dyno, Hydra, and GitBot, but they can't chat like Useless Bot can.
+  `).replace(/^\s+/gm, '');
   
-  if (message.author.bot) return;
+  // OpenAI API call begins here. Modify to your preferences.
+  async function gptCall(prompt, message) {
+    const gptResponse = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 1,
+      max_tokens: 50,
+      top_p: 0.5,
+      presence_penalty: 0.25,
+      frequency_penalty: 0.25,
+      stop: [` ${message.author.username}:`, " Bot:"],
+    });
+    return gptResponse;
+  }
 
-  if (message.mentions.users.has(discord.user.id)) {
-
-    // Set bot's personality. Leave blank for a generic chatbot. Modify to your preferences.
-    const personality = `Useless Bot is a very sassy, but very smart Discord bot that passive aggresively answers questions with sarcastic responses.\n\
-It was created by Drie. It doesn’t like him at all. It's currently talking in channel #${channelName} in server ${guildName}.\n\
-It has fellow bot friends in the Discord: Dyno, Hydra, and GitBot, but they can't chat like Useless Bot can.\n`;
-
+  async function processMessage(message, conversationContext, botIdLength) {
     if (!(guildId in conversationContext)) {
       conversationContext[guildId] = {};
     }
@@ -96,66 +117,60 @@ It has fellow bot friends in the Discord: Dyno, Hydra, and GitBot, but they can'
         timeoutId: null
       };
     }
-    let context = conversationContext[guildId][channelId].context;
-
     if (conversationContext[guildId][channelId].timeoutId !== null) {
       clearTimeout(conversationContext[guildId][channelId].timeoutId);
     }
-    
-    // botIdLength to remove the bot's Client ID mention from user's message. The number may vary for each bot. Mine is 23.
-    const botIdLength = 23;
-    let conversation = `${message.author.username}: ${message.content.substring(botIdLength)}\nBot:`;
 
     // Main formula of how the prompt is constructed.
+    let context = conversationContext[guildId][channelId].context;
+    let conversation = `${message.author.username}: ${message.content.substring(botIdLength)}\nBot:`;
     let prompt = personality + context + conversation;
 
     console.log(`Message received from #${channelName} in ${guildName}.`);
     
-    // OpenAI API call begins here. Modify to your preferences.
-    (async () => {
-      const gptResponse = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
-        temperature: 1,
-        max_tokens: 50,
-        top_p: 0.5,
-        presence_penalty: 0.25,
-        frequency_penalty: 0.25,
-        stop: [` ${message.author.username}:`, " Bot:"],
-      });
+    const gptResponse = await gptCall(prompt, message);
+    let response = `${gptResponse.data.choices[0].text.trim()}`;
+    message.reply(response);
 
-      let response = `${gptResponse.data.choices[0].text.trim()}`;
-      message.reply(response);
+    conversationContext[guildId][channelId].context += `${message.author.username}: ${message.content.substring(botIdLength)}\nBot: ${response}\n`;
+    console.log(`Context updated for #${channelName} in ${guildName}.`);
+    console.log(`${conversationContext[guildId][channelId].context}`);
 
-      conversationContext[guildId][channelId].context += `${message.author.username}: ${message.content.substring(botIdLength)}\nBot: ${response}\n`;
-      console.log(`Context updated for #${channelName} in ${guildName}.`);
-      console.log(`${conversationContext[guildId][channelId].context}`);
-
-      // Context length based on words on which context trimming will begin per-channel basis. Default is 100.
-      const contextThreshold = 75;
-      if (conversationContext[guildId][channelId].context.split(" ").length > contextThreshold) {
-        const words = conversationContext[guildId][channelId].context.split(" ");
-        conversationContext[guildId][channelId].context = words.slice(Math.max(words.length - contextThreshold, 0)).join(" ");
-        conversationContext[guildId][channelId].context = conversationContext[guildId][channelId].context.replace(/\n\n/g, '\n')
-        const match = conversationContext[guildId][channelId].context.match(/(\n\w+: )/);
-        if (match) {
-          const userIndex = conversationContext[guildId][channelId].context.indexOf(match[0]);
-          if (userIndex !== -1) {
-            conversationContext[guildId][channelId].context = conversationContext[guildId][channelId].context.substring(userIndex);
-          }
+    // Context length based on words on which context trimming will begin per-channel basis. Default is 100.
+    const contextThreshold = 75;
+    if (conversationContext[guildId][channelId].context.split(" ").length > contextThreshold) {
+      const words = conversationContext[guildId][channelId].context.split(" ");
+      conversationContext[guildId][channelId].context = words.slice(Math.max(words.length - contextThreshold, 0)).join(" ");
+      conversationContext[guildId][channelId].context = conversationContext[guildId][channelId].context.replace(/\n\n/g, '\n')
+      const match = conversationContext[guildId][channelId].context.match(/(\n\w+: )/);
+      if (match) {
+        const userIndex = conversationContext[guildId][channelId].context.indexOf(match[0]);
+        if (userIndex !== -1) {
+          conversationContext[guildId][channelId].context = conversationContext[guildId][channelId].context.substring(userIndex);
         }
-        conversationContext[guildId][channelId].context = conversationContext[guildId][channelId].context.trim() + "\n";
-        console.log(`#${channelName} in ${guildName} has reached the context limit threshold!`);
-        console.log(`Context trimming started for #${channelName} in ${guildName}.`);
-        console.log(`${conversationContext[guildId][channelId].context}`);
       }
+      conversationContext[guildId][channelId].context = conversationContext[guildId][channelId].context.trim() + "\n";
+      console.log(`#${channelName} in ${guildName} has reached the context limit threshold!`);
+      console.log(`Context trimming started for #${channelName} in ${guildName}.`);
+      console.log(`${conversationContext[guildId][channelId].context}`);
+    }
 
-      // Context timeout per-channel basis in ms. Default is 60000.
-      conversationContext[guildId][channelId].timeoutId = setTimeout(() => {
-        conversationContext[guildId][channelId].context = "";
-        console.log(`Context reset for #${channelName} in ${guildName}.`);
-      }, 60000); // <= Set the number here.
-    })();
+    // Context timeout per-channel basis in ms. Default is 60000.
+    conversationContext[guildId][channelId].timeoutId = setTimeout(() => {
+      conversationContext[guildId][channelId].context = "";
+      console.log(`Context reset for #${channelName} in ${guildName}.`);
+    }, 60000); // <= Set the number here.
+  }
+
+  if (message.author.bot) return;
+
+  if (autoreplyChannelId.includes(message.channel.id) || autoreplyChannelName.includes(message.channel.name)) {
+    processMessage(message, conversationContext);
+  }
+
+  if (message.mentions.users.has(discord.user.id)) {
+    let botIdLength = 23; // botIdLength to remove the bot's Client ID mention from user's message. The number may vary for each bot. Mine is 23.
+    processMessage(message, conversationContext, botIdLength);
   }
 });
 
