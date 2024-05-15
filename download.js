dab69a30a4b5e05f98cd19348d97e5acb3e0245c
Drie <git@drie.cc>
2024-05-16 03:31:50 +0700

    Bot can see pictures now

---
+++ download.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+import https from 'https';
+
+const dirtyUrl = "https://cdn.discordapp.com/attachments/401765372241641474/1240222260497879060/IMG_8308.png?ex=6645c67d&is=664474fd&hm=e2642619ba4801d5bbea1a9dbc49394641f7866e4f7703d4deaa8c0d16dcdff8&";
+const cleanUrl = cleanURL(dirtyUrl);
+const filename = path.basename(cleanUrl);
+const filepath = "./temp/" + filename;
+downloadFile(dirtyUrl, filepath);
+
+function cleanURL(url) {
+  // Use URL object to parse the URL
+  const urlObject = new URL(url);
+  // Return only the origin and pathname
+  return urlObject.origin + urlObject.pathname;
+}
+
+function downloadFile(dirtyUrl, filepath) {
+  https.get(dirtyUrl, (res) => {
+    const fileStream = fs.createWriteStream(filepath);
+    res.pipe(fileStream);
+
+    fileStream.on('finish', () => {
+        fileStream.close();
+        console.log('Download finished.')
+    });
+  })
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import http from 'http';
+import https from 'https';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import { Client, Events, ActivityType, GatewayIntentBits } from 'discord.js';
@@ -182,62 +182,160 @@ client.on(Events.MessageCreate, async message => {
       threshold: HarmBlockThreshold.BLOCK_NONE,
     },
   ];
-  
-  const chatSession = model.startChat({
-    generationConfig,
-    safetySettings,
-    history: []
-  });
 
-  async function generateResponse(chatHistory) {
-    const text = `${botPersonality}### Current chat between ${botNickname} and the users in this channel. Do not imitate any of these users, just send what ${botNickname} would say. Finish ${botNickname}'s reply to this chat without the "${botNickname}: " please:\n${chatHistory}\n${botNickname}: `;
-    const geminiResult = await model.generateContent({
-      contents: [
-        {
-          role: "user",
-          parts: { text: text }
-        }
-      ],
-      generationConfig,
-      safetySettings
-    });
-    return geminiResult;
-  }
+  const mimeTypes = {
+    'png': 'image/png',
+    'jpg': 'image/jpeg',
+    'jpeg': 'image/jpeg',
+    'webp': 'image/webp',
+    'heic': 'image/heic',
+    'heif': 'image/heif',
+    'wav': 'audio/wav',
+    'mp3': 'audio/mp3',
+    'aiff': 'audio/aiff',
+    'aac': 'audio/aac',
+    'ogg': 'audio/ogg',
+    'flac': 'audio/flac',
+    'mp4': 'video/mp4',
+    'mpg': 'video/mpg',
+    'mpeg': 'video/mpeg',
+    'mov': 'video/mov',
+    'avi': 'video/avi',
+    'flv': 'video/x-flv',
+    'webm': 'video/webm',
+    'wmv': 'video/wmv',
+    '3gp': 'video/3gpp'
+  };
 
   async function sendResponse(message) {
-    if (message.attachments.size > 0) {
-      const attachment = message.attachments.first();
-      const fileUrl = attachment.url;    
+    console.log(`\x1b[33m${botNickname} is thinking...\x1b[0m`);
 
-      const prompt = `${userMessage}`;
-      const image = {
-        inlineData: {
-          data: Buffer.from(fs.readFileSync(fileUrl)).toString("base64"),
-          mimeType: "image/png"
+    if (message.attachments.size > 0 < 20) {
+      const attachmentPrompt = `${userName}: ${userMessage}`;
+      const attachment = message.attachments.first();
+      const dirtyUrl = attachment.url;
+      const cleanUrl = cleanURL(dirtyUrl);
+      const filename = path.basename(cleanUrl);
+      const cleanFilename = path.parse(filename).name.toLowerCase()
+        .replace(/^-|-$/g, '') // Remove leading/trailing dashes
+        .replace(/_/g, '-') // Replace underscores with dashes
+        .replace(/[^a-z0-9-]/g, ''); // Remove non-alphanumeric characters;
+      const extension = path.extname(filename).toLowerCase();
+      const filepath = "./temp/" + filename;
+      await downloadFile(dirtyUrl, filepath);
+
+      let mimeType;
+      if (mimeTypes.hasOwnProperty(extension)) {
+        mimeType = mimeTypes[extension];
+      } else {
+        const alternateExtension = extension.slice(1).toLowerCase(); // Remove leading dot
+        if (mimeTypes.hasOwnProperty(alternateExtension)) {
+          mimeType = mimeTypes[alternateExtension];
+        } else {
+          console.error(`Unsupported file type: ${filename}`);
+          // Handle unsupported file
         }
       }
-      processImage(prompt, image);
 
-      async function processImage(prompt, image) {
-        const result = await model.generateContent([prompt, image]);
-        const response = result.response;
-        const imageDescription = response.text();
-        console.log(imageDescription);
+      function cleanURL(url) {
+        // Use URL object to parse the URL
+        const urlObject = new URL(url);
+        // Return only the origin and pathname
+        return urlObject.origin + urlObject.pathname;
+      }
+      
+      function downloadFile(dirtyUrl, filepath) {
+        return new Promise((resolve, reject) => {
+          https.get(dirtyUrl, (res) => {
+            const fileStream = fs.createWriteStream(filepath);
+            res.pipe(fileStream);
+        
+            fileStream.on('finish', () => {
+                fileStream.close();
+                console.log('Download finished.');
+                resolve(); // Resolve the promise on successful download
+            });
+      
+            fileStream.on('error', (error) => {
+              reject(error); // Reject the promise on download error
+            });
+          })
+        });
       }
 
-      // const imageData = await processImageWithGemini(imageUrl);
-      // const imageDescription = await handleImageResponse(imageData);
-    };
-    // console.log(`${botPersonality}`);
-    // console.log(`\x1b[33m--- Chat History---\x1b[0m`);
-    // console.log(chatHistory);
-    // console.log(`${userName}: ${userMessage}`);
-    console.log(`\x1b[33m${botNickname} is thinking...\x1b[0m`);
-    const aiResponse = await generateResponse(chatHistory);
-    const botResponse = aiResponse.response.text().trim();
-    message.channel.send(botResponse);
-    console.log(`${botNickname}: ${botResponse}`);
-    return botResponse;
+      const fileResult = await fileManager.uploadFile(filepath, {
+        mimeType: mimeType,
+        // It will also add the necessary "files/" prefix if not provided
+        name: "files/" + cleanFilename,
+        displayName: cleanFilename
+      });
+
+      async function processAttachment(attachmentPrompt, chatHistory) {
+        const text = `${botPersonality}### Current chat between ${botNickname} and the users in this channel. Do not imitate any of these users, just send what ${botNickname} would say. Finish ${botNickname}'s reply to this chat without the "${botNickname}: " please:`;
+        const result = await model.generateContent({
+          contents: [
+            {
+              role: "user",
+              parts: [
+                { text: text },
+              ],
+            },
+            {
+              role: "user",
+              parts: [
+                { text: chatHistory },
+              ],
+            },
+            {
+              role: "user",
+              parts: [
+                { text: attachmentPrompt },
+                {
+                  fileData: {
+                    mimeType: fileResult.file.mimeType,
+                    fileUri: fileResult.file.uri
+                  }
+                },
+              ],
+            },
+          ],
+        });
+        const response = result.response;
+        const imageDescription = response.text().trim();
+        // console.log(imageDescription);
+        return imageDescription;
+      }
+
+      const aiResponse = await processAttachment(attachmentPrompt, chatHistory);
+      const botResponse = aiResponse;
+      message.channel.send(botResponse);
+      console.log(`${botNickname}: ${botResponse}`);
+      fileManager.deleteFile(fileResult.file.name);
+      return botResponse;
+
+    } else {
+
+      async function generateResponse(chatHistory) {
+        const text = `${botPersonality}### Current chat between ${botNickname} and the users in this channel. Do not imitate any of these users, just send what ${botNickname} would say. Finish ${botNickname}'s reply to this chat without the "${botNickname}: " please:\n${chatHistory}\n${botNickname}: `;
+        const geminiResult = await model.generateContent({
+          contents: [
+            {
+              role: "user",
+              parts: { text: text }
+            }
+          ],
+          generationConfig,
+          safetySettings
+        });
+        return geminiResult;
+      }
+
+      const aiResponse = await generateResponse(chatHistory);
+      const botResponse = aiResponse.response.text().trim();
+      message.channel.send(botResponse);
+      console.log(`${botNickname}: ${botResponse}`);
+      return botResponse;
+    }
   }
 
   function stopAttention(channelId) {
